1ca474312d143f8e784e090938ca0aa22753c374
ksgrit <kubo5680@gmail.com>
2022-09-13 01:07:15 +0900

    first commit

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace APP\Repositories;
+
+use App\Models\User;
+
+class TaskRepository
+{
+    /**
+     * ユーザーのタスク一覧取得
+     *
+     *@param User $user 
+     * @return Collection
+     * 
+     */
+    public function forUser(User $user)
+    {
+        return $user->tasks()
+        ->orderBy('created_at','asc')
+        ->get();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\HelloController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks');
+Route::post('/task', [App\Http\Controllers\TaskController::class, 'store'])->name('task');
+Route::delete('/task/{task}', [App\Http\Controllers\TaskController::class, 'destroy'])->name('/task/{task}');
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
